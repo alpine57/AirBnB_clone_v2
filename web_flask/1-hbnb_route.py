60504e76a0fd17d2de333ebfd6731edbba8cbc6b
alpine57 <kamutileeon@gmail.com>
2024-06-01 09:03:47 -0700

    done

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/pyhon3
-from flask import Flask
 """ script that starts a Flask web application"""
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
